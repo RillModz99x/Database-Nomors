c11b894176276549232ef4e262a0d3b2e6564e75
RillModz99x <rilljerami580@gmail.com>
2025-10-12 06:56:28 +0700

    Create axeploit.js

---
+++ db/axeploit.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+  const url = "https://raw.githubusercontent.com/RillModz99x/Database-Nomors/main/dbv7.json";
+  try {
+    const r = await fetch(url);
+    const data = await r.json();
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ error: "Gagal ambil data" });
+  }
+}
